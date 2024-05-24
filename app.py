1d1c00c0cf6c21e8642823db8d2fa4fe23bf5bb9
Cesar Bouli <contato@bouli.com.br>
2024-05-24 01:07:38 -0300

    feat(byeceebee): Add ByeCeeBee artist

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    domain = "cesarbouli.com"
-    artist_name = "Cesar Bouli"
-    meta_text = f"Ouça {artist_name}."
-    meta_title = f"Ouça {artist_name}."
-    meta_url = "http://cesarbouli.com"
-    cta = 'Ouça o Disco "Cesar Bouli"'
-    cta_url = "cesarbouli"
-    soundcloud_link = "https://soundcloud.com/cesar-bouli"
-    facebook_link = "https://www.facebook.com/cesarbouli"
-    instagram_link = "https://www.instagram.com/cesarbouli"
-    youtube_link = "https://www.youtube.com/cesarbouli"
-    spotify_link = "https://open.spotify.com/artist/16K0CW2ObGqglDFtWjxbMb"
+    if request.headers['Host'] == "cesarbouli.com":
+        domain = request.headers['Host']
+        artist_name = "Cesar Bouli"
+        meta_text = f"Ouça {artist_name}."
+        meta_title = f"Ouça {artist_name}."
+        meta_url = f"http://{domain}"
+        cta = 'Ouça o Disco "Cesar Bouli"'
+        cta_url = "cesarbouli"
+        soundcloud_link = "https://soundcloud.com/cesar-bouli"
+        facebook_link = "https://www.facebook.com/cesarbouli"
+        instagram_link = "https://www.instagram.com/cesarbouli"
+        youtube_link = "https://www.youtube.com/cesarbouli"
+        spotify_link = "https://open.spotify.com/artist/16K0CW2ObGqglDFtWjxbMb"
+    else:
+        domain = "byeceebee.com"
+        artist_name = "ByeCeeBee"
+        meta_text = f"Listen to {artist_name}."
+        meta_title = f"Listen to {artist_name}."
+        meta_url = f"http://{domain}"
+        cta = 'Listen to "Diana"'
+        cta_url = "diana"
+        soundcloud_link = False
+        facebook_link = False
+        instagram_link = "https://www.instagram.com/byeceebee"
+        youtube_link = "https://www.youtube.com/@ByeCeeBee"
+        spotify_link = "https://open.spotify.com/artist/6sJjhXfbYaWZXeVL81BZiz"
 
+    #print(request.url_root)
+    #print(request.headers['Host'])
 
     return render_template(
         "index.jinja",
@@ -35,6 +51,9 @@ def index():
         spotify_link=spotify_link,
     )
 
+@app.route("/diana")
+def diana():
+    return redirect("https://open.spotify.com/track/7GPSPivgaqYPQ6FstxXi9C")
 
 @app.route("/cesar-bouli")
 def cesarbouli():
