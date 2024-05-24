69adad08d272a98147c62cf3bbcd6913e2dcf16a
Cesar Bouli <contato@bouli.com.br>
2024-05-23 21:18:59 -0300

    chore(linter): Beautify

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    artist_name = 'Cesar Bouli'
-    meta_image = 'http://cesarbouli.com/images/photo.jpg'
-    meta_text = f'Ouça {artist_name}.'
-    meta_title = f'Ouça {artist_name}.'
+    artist_name = "Cesar Bouli"
+    meta_image = "http://cesarbouli.com/images/photo.jpg"
+    meta_text = f"Ouça {artist_name}."
+    meta_title = f"Ouça {artist_name}."
     cta = 'Ouça o Disco "Cesar Bouli"'
     cta_url = "cesarbouli"
 
-    return render_template('index.jinja', artist_name=artist_name ,meta_image=meta_image , meta_text=meta_text , meta_title = meta_title, cta=cta, cta_url=cta_url)
+    return render_template(
+        "index.jinja",
+        artist_name=artist_name,
+        meta_image=meta_image,
+        meta_text=meta_text,
+        meta_title=meta_title,
+        cta=cta,
+        cta_url=cta_url,
+    )
 
 
-@app.route('/cesar-bouli')
+@app.route("/cesar-bouli")
 def cesarbouli():
-    return redirect('https://distrokid.com/hyperfollow/cesarbouli/cesar-bouli')
+    return redirect("https://distrokid.com/hyperfollow/cesarbouli/cesar-bouli")
 
 
-@app.route('/hey-irmao')
+@app.route("/hey-irmao")
 def heyirmao():
-    return redirect('https://distrokid.com/hyperfollow/cesarbouli/hey-irmo-2')
+    return redirect("https://distrokid.com/hyperfollow/cesarbouli/hey-irmo-2")
 
 
-@app.route('/como-se-fosse-a-ultima-vez')
+@app.route("/como-se-fosse-a-ultima-vez")
 def comosefosseaultimavez():
-    return redirect('http://cesarbouli.com/como-se-fosse-a-ultima-vez')
+    return redirect("http://cesarbouli.com/como-se-fosse-a-ultima-vez")
 
 
-@app.route('/vale-a-pena')
+@app.route("/vale-a-pena")
 def valeapena():
-    return redirect('https://distrokid.com/hyperfollow/cesarbouli/vale-a-pena-4')
+    return redirect("https://distrokid.com/hyperfollow/cesarbouli/vale-a-pena-4")
