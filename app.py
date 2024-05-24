c6205022bc320c7accdfe92bc84fea9b6a20f423
Cesar Bouli <contato@bouli.com.br>
2024-05-23 23:31:31 -0300

    chore(refactor): Refactor to a generic structure

--- app.py
+++ app.py
@@ -5,21 +5,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    domain = "cesarbouli.com"
     artist_name = "Cesar Bouli"
-    meta_image = "http://cesarbouli.com/images/photo.jpg"
     meta_text = f"Ouça {artist_name}."
     meta_title = f"Ouça {artist_name}."
+    meta_url = "http://cesarbouli.com"
     cta = 'Ouça o Disco "Cesar Bouli"'
     cta_url = "cesarbouli"
+    soundcloud_link = "https://soundcloud.com/cesar-bouli"
+    facebook_link = "https://www.facebook.com/cesarbouli"
+    instagram_link = "https://www.instagram.com/cesarbouli"
+    youtube_link = "https://www.youtube.com/cesarbouli"
+    spotify_link = "https://open.spotify.com/artist/16K0CW2ObGqglDFtWjxbMb"
+
 
     return render_template(
         "index.jinja",
+        domain=domain,
         artist_name=artist_name,
-        meta_image=meta_image,
         meta_text=meta_text,
         meta_title=meta_title,
+        meta_url=meta_url,
         cta=cta,
         cta_url=cta_url,
+        soundcloud_link=soundcloud_link,
+        facebook_link=facebook_link,
+        instagram_link=instagram_link,
+        youtube_link=youtube_link,
+        spotify_link=spotify_link,
     )
 
 
