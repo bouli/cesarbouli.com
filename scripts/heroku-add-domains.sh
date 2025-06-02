20b19171d1d32185ac2c80d9f2ee2cad97dcb9d6
Cesar Bouli <contato@bouli.com.br>
2025-06-02 12:56:00 -0300

    feat: Add scripts folder with the domains add

---
+++ scripts/heroku-add-domains.sh
@@ -0,0 +1,6 @@
+heroku domains:add byeceebee.com
+heroku domains:add "*.byeceebee.com"
+heroku domains:add salviasupernova.com.br
+heroku domains:add "*.salviasupernova.com.br"
+heroku domains:add cesarbouli.com
+heroku domains:add "*.cesarbouli.com"
